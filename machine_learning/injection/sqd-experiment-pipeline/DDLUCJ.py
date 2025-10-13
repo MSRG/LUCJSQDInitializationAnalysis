c5095579ed83213965994e9b35802ff1c713f50c
ChemRacer <grier_jones@yahoo.com>
2025-10-13 15:32:01 -0400

    prep DRAC postproc runs

--- machine_learning/injection/sqd-experiment-pipeline/DDLUCJ.py
+++ machine_learning/injection/sqd-experiment-pipeline/DDLUCJ.py
@@ -1,3 +1,44 @@
+import psutil
+from functools import partial
+import sys
+# !{sys.executable} --version
+# !{sys.executable} -m pip install shap --upgrade 
+import joblib
+import time
+from shutil import copy
+import numpy as np
+import pandas as pd
+#import tensorflow as tf
+import os
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import pickle
+# import xgboost as xgb
+
+from glob import glob
+# import psi4
+# from helper_CC_ML_spacial import *
+
+import pyscf
+from pyscf import gto, scf, mcscf, cc
+
+import ffsim
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+
+from qiskit_addon_sqd.fermion import SCIResult, diagonalize_fermionic_hamiltonian
+
+from ansatzmap import get_zigzag_physical_layout
+
+
 class DDLUCJ:
     def __init__(self,StructurePath, 
                  BasisSet, 
@@ -300,7 +341,7 @@ class DDLUCJ:
             sci_solver = partial(solve_sci_batch, spin_sq=self.Spin, max_cycle=self.max_cycle)
         else:
             from qiskit_addon_dice_solver import solve_sci_batch
-            sci_solver = partial(solve_sci_batch, spin_sq=self.Spin, max_cycle=self.max_cycle,mpirun_options= ["-quiet", "-n", f"{self.n_jobs}"],temp_dir="./",clean_temp_dir=False)
+            sci_solver = partial(solve_sci_batch, spin_sq=self.Spin, max_cycle=self.max_cycle,mpirun_options= ["-quiet", "-n", f"{self.n_jobs}"],temp_dir=self.temp_dir,clean_temp_dir=self.clean_temp_dir)
         # List to capture intermediate results
         result_history = []
         
