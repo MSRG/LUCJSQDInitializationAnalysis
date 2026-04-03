4679eed4d18ab3bbcc45ea90a545834fd13ece55
ChemRacer <grier_jones@yahoo.com>
2026-04-03 10:24:46 -0400

    edit submitall_date.sh

--- machine_learning/injection/DDLUCJ-experiment-pipeline/submitall_date.sh
+++ machine_learning/injection/DDLUCJ-experiment-pipeline/submitall_date.sh
@@ -2,17 +2,25 @@
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 runfiles=$(find "${SCRIPT_DIR}/postprocess" -name "*.sh")
-
+echo $SCRIPT_DIR
 for i in $runfiles; do
     filename_no_extension=$(basename "${i%.*}")
     file2="${SCRIPT_DIR}/energies/${filename_no_extension}.txt"
 
     if [[ -f "$i" && -e "$file2" ]]; then
         file_month=$(stat -c "%y" "$file2" | cut -c6-7)
+
         if [[ "$file_month" == "10" ]]; then
+	    echo "$file_month"
+  	    cd ./postprocess
             sbatch "$i"
+  	    cd ../
+            echo "$i"
         fi
     elif [[ -f "$i" && ! -e "$file2" ]]; then
-        sbatch "$i"
+  	    cd ./postprocess
+            sbatch "$i"
+  	    cd ../
+        echo "$i"
     fi
 done
