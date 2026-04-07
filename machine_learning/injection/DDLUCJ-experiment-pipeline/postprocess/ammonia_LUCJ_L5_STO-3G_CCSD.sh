7395fd7e1131eca9aefb716d6f8bd23566432356
ChemRacer <grier_jones@yahoo.com>
2026-04-07 17:17:57 -0400

    new scripts 3.0... hopefully this fixes the account issue

--- machine_learning/injection/DDLUCJ-experiment-pipeline/postprocess/ammonia_LUCJ_L5_STO-3G_CCSD.sh
+++ machine_learning/injection/DDLUCJ-experiment-pipeline/postprocess/ammonia_LUCJ_L5_STO-3G_CCSD.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 #SBATCH --time=1-0:00:00
+#SBATCH -J ammonia_LUCJ_L5_STO-3G_CCSD
 #SBATCH --account=rrg-jacobsen-ab
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
 #SBATCH --mem-per-cpu=1000M
-#SBATCH --job-name=ammonia_LUCJ_L5_STO-3G_CCSD
 #SBATCH --error=job.e%J
 #SBATCH --output=job.o%j
 
 
 
 echo 'About to run python file'
+echo 'About to run python file'
 module load python/3.10
+
+module load StdEnv/2023
 module load openmpi
 module load symengine rust
 module load hdf5
 module load openblas
-source /lustre09/project/6004825/gjones/ENV/bin/activate
+
+echo "TEMP DIR: $SLURM_TMPDIR"
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install -e /home/gjones/projects/def-jacobsen/gjones/qiskit-addon-dice-solver/
+
+
 export LD_LIBRARY_PATH=$EBROOTOPENBLAS/lib:$LD_LIBRARY_PATH
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
